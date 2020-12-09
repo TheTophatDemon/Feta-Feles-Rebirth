98d31f80ecc9c3cbda66c098798e3f88c1c51fae
The Tophat Demon <andsotherewas@gmail.com>
2020-12-08 23:01:34 -0600

    Simplified sprite code so that matrix is stored in the struct and calculated at creation

--- sprite.go
+++ sprite.go
@@ -1,40 +1,44 @@
 package main
 
 import (
+	"image"
 	"math"
 
 	"github.com/hajimehoshi/ebiten"
 )
 
-//Todo: Optimize by keeping track of previous state and recalculating transform matrix
-//Todo: Remove middleman and store sub-image instead of src
-
 type Sprite struct {
 	subImg *ebiten.Image
-	ofs    *Vec2f
-	flipH  bool
-	flipV  bool
-	orient int
+	matrix *ebiten.GeoM
+}
+
+func NewSprite(src image.Rectangle, ofs *Vec2f, flipH, flipV bool, orient int) *Sprite {
+	subImg := graphics.SubImage(src).(*ebiten.Image)
+	matrix := new(ebiten.GeoM)
+
+	//Perform rotation and scaling with respect to the center
+	hw := float64(subImg.Bounds().Dx()) / 2.0
+	hh := float64(subImg.Bounds().Dy()) / 2.0
+	matrix.Translate(-hw, -hh)
+	scx := 1.0
+	if flipH {
+		scx = -1.0
+	}
+	scy := 1.0
+	if flipV {
+		scy = -1.0
+	}
+	matrix.Scale(scx, scy)
+	matrix.Rotate(float64(orient) * math.Pi / 2.0)
+	matrix.Translate(hw, hh)
+	matrix.Translate(ofs.x, ofs.y)
+
+	return &Sprite{subImg, matrix}
 }
 
 func (spr *Sprite) Draw(target *ebiten.Image, pt *ebiten.GeoM) {
 	op := &ebiten.DrawImageOptions{}
-	//Perform rotation and scaling with respect to the center
-	hw := float64(spr.subImg.Bounds().Dx()) / 2.0
-	hh := float64(spr.subImg.Bounds().Dy()) / 2.0
-	op.GeoM.Translate(-hw, -hh)
-	scx := 1.0
-	if spr.flipH {
-		scx = -1.0
-	}
-	scy := 1.0
-	if spr.flipV {
-		scy = -1.0
-	}
-	op.GeoM.Scale(scx, scy)
-	op.GeoM.Rotate(float64(spr.orient) * math.Pi / 2.0)
-	op.GeoM.Translate(hw, hh)
-	op.GeoM.Translate(spr.ofs.x, spr.ofs.y)
+	op.GeoM = *spr.matrix
 	op.GeoM.Concat(*pt)
 	target.DrawImage(spr.subImg, op)
 }
